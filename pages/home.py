afc90c3cdf9f7edf317d3274b0ef614776bcdc50
Mohamed Elauzei <mohamedelauzei@gmail.com>
2024-02-14 07:04:57 +0200

    aboghris logo and tab icon for aboghris on render

--- pages/home.py
+++ pages/home.py
@@ -7,8 +7,8 @@ dash.register_page(__name__, path="/")
 
 layout = html.Div(
     [
-        html.H1('MEDAD SOFT', className="text-center mt-3 fw-bolder", style={'color':'#2471a1'}),
-        html.Img(src='assets/midad.png',style={'height':'500px', 'width':'60%'}, className="img-fluid mx-auto d-block img-responsive my-5")
+        html.H1('ABOGHRIS SOFT', className="text-center mt-3 fw-bolder", style={'color':'#2471a1'}),
+        html.Img(src='assets/aboghris.jpg',style={'height':'500px', 'width':'60%'}, className="img-fluid mx-auto d-block img-responsive my-5")
         # dcc.Link("Go to Page 1", href="/page-1"),
         # html.Br(),
         # dcc.Link("Go to Page 2", href="/page-2"),
